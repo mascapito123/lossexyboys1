c66f01a6494bf1d7c93eeff141d6dca7c9ab5ba3
camargogarnicafernando-ctrl <camargogarnicafernando@pedropoveda.edu.bo>
2026-05-19 14:26:44 -0400

    Mas_cambios

--- registro.php
+++ registro.php
@@ -222,7 +222,6 @@ font-size: 16px;
   <div class="dropdown-content">
     <a href="https://maps.app.goo.gl/L1Kd1FhKZzSNHeTV7">Ubicación</a>
     <a href="registro.php">Registrarse</a>
-    <a href="sexiboys.php">Usuarios</a>
     <a href="creacion.php">Creación de vasos</a>
     <a href="creacionjugos.php">Creación de jugos</a>
     <a href="adminbueno.php">Panel de Administración</a>
