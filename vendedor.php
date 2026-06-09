d13b5ab1fc059e80be1b6dbcba4e33f0a396257b
castellonarcienegaisac-a11y <castellonarcienegaisac@pedropoveda.edu.bo>
2026-06-09 14:30:23 -0400

    php_isac

--- vendedor.php
+++ vendedor.php
@@ -152,24 +152,21 @@ section.cuadro{
 </head>
 <body>
     <header>
-  <a href="pagina_principal.html">
+  <a href="pagina_principal.php">
   <img src="logo bomble.png" alt="Logo" class="logo" style="border-radius: 50%;">
   </a>
   <nav>
     <ul>
-      <li><a href="admin.html">NUESTROS JUGOS</a></li>
-      <li><a href="vendedor.html">PROGRAMAS</a></li>
-      <li><a href="combos.html">COMBOS</a></li>
-      <li><a href="quienes.html">QUIENES SOMOS</a></li>
-      <li><a href="telefonos .html">TELEFONOS</a></li>
+      <li><a href="productos.php">NUESTROS JUGOS</a></li>
+      <li><a href="horarioatencion.php">PROGRAMAS</a></li>
+      <li><a href="combos.php">COMBOS</a></li>
+      <li><a href="quienes.php">QUIENES SOMOS</a></li>
+      <li><a href="telefonos.php">TELEFONOS</a></li>
       <li class="dropdown">
-  <a href="productos.html">MÁS</a>
+  <a href="productos.php">MÁS</a>
   <div class="dropdown-content">
-    <a href="ubicacion.html">Ubicación</a>
-    <a href="registro.html">Registrarse</a>
-    <a href="creacion.php">Creacion de vasos</a>
-    <a href="creacionjugos.php">Creacion de jugos</a>
-    <a href="adminbueno.php">Panel de Administracion</a>
+    <a href="ubicacion.php">Ubicación</a>
+    <a href="registro.php">Registrarse</a>
   </div>
 </li>
     </ul>
