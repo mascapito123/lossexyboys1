0d8b1c0c48e97ca5c2f0a8e4b610180d6bd2bdeb
manto88 <hacielduzan@gmail.com>
2026-06-12 12:19:29 -0400

    malagaxd

--- horarioatencion.php
+++ horarioatencion.php
@@ -116,7 +116,7 @@ background-color: #c5fff5;
 <body>
 <header>
   <a href="pagina_principal.php">
-  <img src="logo bomble.png" alt="Logo" class="logo" style="border-radius: 50%;">
+  <img src="imagenes/logo bomble.png" alt="Logo" class="logo" style="border-radius: 50%;">
   </a>
   <nav>
     <ul>
--- productos.php
+++ productos.php
@@ -149,7 +149,7 @@
 
 <header>
   <a href="pagina_principal.php">
-  <img src="logo bomble.png" alt="Logo" class="logo" style="border-radius: 50%;">
+  <img src="imagenes/logo bomble.png" alt="Logo" class="logo" style="border-radius: 50%;">
   </a>
   <nav>
     <ul>
