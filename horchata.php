f3b1559e6c68c320110f590f7f345c8b12456ed6
matiass-msa <castellonarcienegaisac@pedropoveda.edu.bo>
2026-06-16 13:55:22 -0400

    hola

--- horchata.php
+++ horchata.php
@@ -24,76 +24,6 @@
     grid-template-columns: auto 1fr;
     align-items: center;
   }
-  header {
-    background: #111;
-    color: #fff;
-    padding: 12px 40px;
-    display: grid;
-    grid-template-columns: auto 1fr;
-    align-items: center;
-  }
-  header img.logo {
-    height: 100px;
-    width: 100px;
-  }
-  nav ul {
-    display: grid;
-    grid-auto-flow: column;
-    justify-content: center;
-    list-style: none;
-    gap: 70px;
-  }
-  nav a {
-    color: #fff;
-    text-decoration: none;
-  }
-  nav a:hover {
-    color: #5eff6ce5;
-  }
-    .container {
-            max-width: 800px;
-            margin: auto;
-            padding: 40px;
-            color: white;
-            text-align: center;
-    }
-    .container2 {
-            max-width: 800px;
-            margin: auto;
-            padding: 40px;
-            text-align: center;
-            color: rgb(127, 255, 170);
-    }
-    .dropdown {
-  position: relative;
-  display: inline-block;
-}
-
-.dropdown-content {
-  display: none;
-  position: absolute;
-  background-color: #111;
-  min-width: 160px;
-  box-shadow: 0 8px 16px rgba(0,0,0,0.2);
-  z-index: 1;
-  border-radius: 5px;
-}
-
-.dropdown-content a {
-  color: #fff;
-  padding: 12px 16px;
-  text-decoration: none;
-  display: block;
-}
-
-.dropdown-content a:hover {
-  background-color: #333;
-  color: #5eff6ce5;
-}
-
-.dropdown:hover .dropdown-content {
-  display: block;
-}
     .sections {
     display: grid;
     grid-template-columns: repeat(auto-fit, minmax(320px, 1fr));
@@ -154,9 +84,9 @@ include ("encabezado.php");
   <section class="sections">
   <div class="reinaduende">
     <a href="comprar.php?jugo=horchata&precio=5"><img src="https://images.aws.nestle.recipes/original/8eaf50148ed521383df5d9793cba995f_whatsapp_image_2022-04-28_at_1.00.26_pm_(2).jpeg" alt="Programas Hábitos"></a>
-    <h2>Jugo de Horchata</h2><br>
   </div>
 </section>
+<h2>Jugo de horchata</h2>
     <?php
      include("pie_pg.php");
     ?>
