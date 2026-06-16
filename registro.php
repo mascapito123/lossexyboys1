cc1d5ed5ffba9df67f5c4043584aa7b2532f14a8
matiass-msa <castellonarcienegaisac@pedropoveda.edu.bo>
2026-06-16 14:32:34 -0400

    header

--- registro.php
+++ registro.php
@@ -11,63 +11,6 @@
       padding: 0;
       font-family: Arial, Helvetica, sans-serif;
     }
-    header {
-      background: #111;
-      color: #fff;
-      padding: 12px 40px;
-      display: grid;
-      grid-template-columns: auto 1fr;
-      align-items: center;
-    }
-    header img.logo {
-      height: 100px;
-      width: 100px;
-      border-radius: 50%;
-    }
-    nav ul {
-      display: grid;
-      grid-auto-flow: column;
-      justify-content: center;
-      list-style: none;
-      gap: 70px;
-    }
-    nav a {
-      color: #fff;
-      text-decoration: none;
-    }
-    nav a:hover {
-      color: #5eff6ce5;
-    }
-  .dropdown {
-  position: relative;
-  display: inline-block;
-}
-
-.dropdown-content {
-  display: none;
-  position: absolute;
-  background-color: #111;
-  min-width: 160px;
-  box-shadow: 0 8px 16px rgba(0,0,0,0.2);
-  z-index: 1;
-  border-radius: 5px;
-}
-
-.dropdown-content a {
-  color: #fff;
-  padding: 12px 16px;
-  text-decoration: none;
-  display: block;
-}
-
-.dropdown-content a:hover {
-  background-color: #333;
-  color: #5eff6ce5;
-}
-
-.dropdown:hover .dropdown-content {
-  display: block;
-}
     .contenido {
       display: flex;
       flex-wrap: wrap;
@@ -149,31 +92,10 @@ font-size: 16px;
   </style>
 </head>
 <body>
-  <header>
-    <a href="pagina_principal.php">
-      <img src="imagenes/logo bomble.png" alt="Logo" class="logo">
-    </a>
-    <nav>
-      <ul>
-        <li><a href="productos.php">NUESTROS JUGOS</a></li>
-        <li><a href="horarioatencion.php">PROGRAMAS</a></li>
-        <li><a href="quienes.php">QUIENES SOMOS</a></li>
-        <li><a href="telefonos .php">TELEFONOS</a></li>
-   <li class="dropdown">
-  <a href="#">MÁS</a>
-  <div class="dropdown-content">
-    <a href="https://maps.app.goo.gl/L1Kd1FhKZzSNHeTV7">Ubicación</a>
-    <a href="registro.php">Registrarse</a>
-    <a href="creacion.php">Creación de vasos</a>
-    <a href="creacionjugos.php">Creación de jugos</a>
-    <a href="adminbueno.php">Panel de Administración</a>
-  </div>
-</li>
-      </ul>
-    </nav>
-  </header>
+    <?php
+include ("encabezado.php");
+?>
 
-    
 </section>
 <h1>REGISTRATE EN BOOMBLE</h1>
 <form action="create_registro.php" method="post">
